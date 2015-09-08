3041e74ec22974b0099564dd15886cdec38ca06f
liugang <liugang@centos.com>
2015-09-08 15:20:16 +0800

    update

--- include/header.h
+++ include/header.h
@@ -1,9 +1,12 @@
+#ifndef __HEADER__
+#define __HEADER__
 /* c1.h (程序名) */
 #include<string.h>
 #include<ctype.h>
 #include<limits.h> /* INT_MAX等 */
 #include<stdio.h> /* EOF(=^Z或F6),NULL */
 #include<stdlib.h> /* atoi() */
+#include<stdarg.h>
 #ifdef __APPLE__
   #include<malloc/malloc.h> /* malloc()等 */
   #include <sys/uio.h>
@@ -22,4 +25,5 @@
 /* #define OVERFLOW -2 因为在math.h中已定义OVERFLOW的值为3,故去掉此行 */
 typedef int Status; /* Status是函数的类型,其值是函数结果状态代码，如OK等 */
 typedef int Boolean; /* Boolean是布尔类型,其值是TRUE或FALSE */
+#endif
 
